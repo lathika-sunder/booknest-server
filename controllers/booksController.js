3674633eaf3b883a120b34b391589c021340407a
Lathika Sunder <lathikasunder11@gmail.com>
2024-06-28 13:31:17 +0530

    Add -Book Controllers Implemented

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,13 +1,33 @@
+const Book =require('../models/bookModel')
 
-const searchBooks = (req, res) => {
-    // Logic to search books
-    res.send('Books searched');
+const searchBooks = async (request, response) => {
+    const { genre,author } = request.body
+    
+    try {
+        const result = await Bus.find({
+            $or: [
+                { genre: genre }
+            ],
+            $or: [
+                { author:author }
+            ]
+        });
+        response.status(200).json(result)
+    } catch (error) {
+        response.status(500).json({ message: "Error getting Books", error: error.message })
+    }
+}
+
+// Get All Books
+const getBooks = async (request, response) => {
+    try {
+        const books = await Book.find();
+        response.status(200).json({ message: "All books retrieved", books });
+    } catch (error) {
+        response.status(500).json({ message: "Internal Server Error", error: error.message });
+    }
 };
 
-const getBooks = (req, res) => {
-    // Logic to get all books
-    res.send('Books retrieved');
-};
 
 const getLendedBooks = (req, res) => {
     // Logic to get lended books
@@ -19,10 +39,28 @@ const viewDues = (req, res) => {
     res.send('Dues viewed');
 };
 
-const addBook = (req, res) => {
-    // Logic to add a book
-    res.send('Book added');
+// Add Book
+const addBook = async (request, response) => {
+    const { bookName, author, title, publisher, genre, language, publishedDate } = request.body;
+
+    const newBook = new Book({
+        bookName,
+        author,
+        title,
+        publisher,
+        genre,
+        language,
+        publishedDate
+    });
+
+    try {
+        const savedBook = await newBook.save();
+        response.status(201).json({ message: "Book added successfully", book: savedBook });
+    } catch (error) {
+        response.status(400).json({ message: "Failed to add book", error: error.message });
+    }
 };
+
 
 const removeBook = (req, res) => {
     // Logic to remove a book
